60cdc3f64d8f40e2203c1ef56ef619f17b934e0a
Roger Vendrell Colet <47077975+rogervendrell@users.noreply.github.com>
2025-12-04 12:53:35 +0100

    Update fusion.py

--- ultralytics/nn/modules/fusion.py
+++ ultralytics/nn/modules/fusion.py
@@ -1,10 +1,8 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-
-class Fusion_pairs(nn.Module):
+class Fusion(nn.Module):
     def __init__(self, embed_dim, num_heads=1):
         super().__init__()
         self.cross_attn = nn.MultiheadAttention(
@@ -24,58 +22,12 @@ class Fusion_pairs(nn.Module):
         if tpe is None: return vpe
         if vpe is None: return tpe
 
+        assert tpe.shape == vpe.shape, \
+            f"got mismatching shapes for tpe {tpe.shape} and vpe {vpe.shape}"
+
         fused, _ = self.cross_attn(
             query=tpe.unsqueeze(0),
             key=vpe.unsqueeze(0),
             value=vpe.unsqueeze(0),
         )
         return fused.squeeze(0)
-
-
-
-class Fusion(nn.Module):
-    def __init__(self, N, embed_dim, num_heads=1):
-        super().__init__()
-        self.embed_dim = embed_dim
-        self.num_heads = num_heads
-        
-        self.cross_attn = nn.MultiheadAttention(
-            embed_dim=embed_dim,
-            num_heads=num_heads,
-            batch_first=False # (N, B, E)
-        )
-        
-        # Create diagonal mask
-        self.register_buffer("_diag_mask", torch.full((N, N), float('-inf')))
-        self._diag_mask = torch.diag_embed(torch.diag(self._diag_mask), offset=0)
-
-    def forward(self, tpe, vpe_padded):
-        """
-        Applies masked cross-attention for pair-wise fusion.
-
-        Args:
-            tpe: Textual embeddings (Query). Shape: (B, 80, E)
-            vpe_padded: Padded visual embeddings (Key/Value). Shape: (B, 80, E)
-
-        Returns:
-            fused: Fused embeddings. Shape: (B, 80, E)
-        """
-        B, N, E = tpe.shape
-
-        query = tpe.permute(1, 0, 2)
-        key = vpe_padded.permute(1, 0, 2)
-        value = vpe_padded.permute(1, 0, 2)
-
-        # MHA expects a mask of shape (B*H, N, N).
-        # Repeat diagonal mask across the batch and heads
-        attn_mask_batch = self._diag_mask.unsqueeze(0).repeat(B * self.num_heads, 1, 1)
-
-        fused, _ = self.cross_attn(
-            query=query,
-            key=key,
-            value=value,
-            attn_mask=attn_mask_batch
-        )
-        
-        # 4. Permute back to the standard (B, N, E)
-        return fused.permute(1, 0, 2)
