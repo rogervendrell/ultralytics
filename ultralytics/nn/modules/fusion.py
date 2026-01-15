df662b5e5ef27551f11c4e353db63bb2e61dbf58
Roger Vendrell Colet <47077975+rogervendrell@users.noreply.github.com>
2026-01-15 10:01:58 +0100

    Add query<->key,value interchangeability

--- ultralytics/nn/modules/fusion.py
+++ ultralytics/nn/modules/fusion.py
@@ -3,12 +3,14 @@ import torch.nn.functional as F
 
 
 class Fusion(nn.Module):
-    def __init__(self, embed_dim, num_heads=1):
+    def __init__(self, embed_dim, num_heads=1, query="tpe"):
         super().__init__()
         self.cross_attn = nn.MultiheadAttention(
             embed_dim=embed_dim,
             num_heads=num_heads,
         )
+        assert query in ["tpe", "vpe"]
+        self.query = query
 
     def forward(self, tpe, vpe):
         """
@@ -25,9 +27,17 @@ class Fusion(nn.Module):
         assert tpe.shape == vpe.shape, \
             f"got mismatching shapes for tpe {tpe.shape} and vpe {vpe.shape}"
 
-        fused, _ = self.cross_attn(
-            query=tpe.unsqueeze(0),
-            key=vpe.unsqueeze(0),
-            value=vpe.unsqueeze(0),
-        )
+        if self.query == "tpe":
+            fused, _ = self.cross_attn(
+                query=tpe.unsqueeze(0),
+                key=vpe.unsqueeze(0),
+                value=vpe.unsqueeze(0),
+            )
+        else:
+            fused, _ = self.cross_attn(
+                query=vpe.unsqueeze(0),
+                key=tpe.unsqueeze(0),
+                value=tpe.unsqueeze(0),
+            )
+            
         return fused.squeeze(0)
